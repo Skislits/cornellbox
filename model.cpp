fa4d637b531a8da7f4b6b3e472d49a5656d5fba8
Сергей Кислицын <KislitsynS@yandex.ru>
2023-05-21 19:15:20 +0500

    delete unused comments in model

--- model.cpp
+++ model.cpp
@@ -1,13 +1,5 @@
 #include "model.h"
 
-//Rectangle::Rectangle(vector<vec3> rectangle_corners, vec3 RGB_color) :
-//    rectangle_corners(rectangle_corners), RGB_color(RGB_color) {}
-//
-//const vec3& Rectangle::operator[](const size_t& index) const {
-//	return rectangle_corners[index];
-//}
-//
-//Block::Block(vector<Rectangle> block_sides) : block_sides(block_sides) {}
 using std::vector;
 using glm::vec3;
 
