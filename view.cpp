e8c231b646fd7c727acc7da5e1432d60bfb231d7
Сергей Кислицын <KislitsynS@yandex.ru>
2023-05-21 14:42:12 +0500

    draw a triangle

--- view.cpp
+++ view.cpp
@@ -29,9 +29,47 @@ View::View(Model& model) : model(model) {
 
 	// tell OpenGL to start using program
 	glUseProgram(shader_program);
+
+	// create vertex array object (VAO) for storing settings
+	// VAO stores how data should be read, what format the data is, 
+	// were it should go in the shaders, etc., but it doesn't store actual data
+	GLuint triangle_settings;
+	glGenVertexArrays(1, &triangle_settings);
+	glBindVertexArray(triangle_settings);
+
+	// each line = vertex
+	// first three values are xyz coordinates and the last three values are RGB colors
+	float triangle_data[] = {
+		-0.5, -0.5, 0, 1., 0, 0,
+		 0.5, -0.5, 0, 1., 0, 0,
+		 0.5,  0.5, 0, 1., 0, 0
+	};
+
+	// create vertex buffer object (VBO), get a handle to the buffer, load triangle vertices into the buffer
+	GLuint triangle_buffer;
+	glGenBuffers(1, &triangle_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, triangle_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_data), triangle_data, GL_STATIC_DRAW);
+	
+	// get a handle to the "vertex_position" input attribute, tell it to read 3 floats at a time
+	// for the variable related to vertex_position_attribute, give it three data values
+	// data of the form floats, data which shouldn't be normalized, data which comes in chunks of 6
+	// take the three data values stating from byte 0 in such a chunk
+	GLint vertex_position_attribute = glGetAttribLocation(shader_program, "vertex_position");
+	glVertexAttribPointer(vertex_position_attribute, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(vertex_position_attribute);
+
+	// get a handle to the "color_input" input attribute, tell it to read 3 floats at a time
+	GLint color_in_attribute = glGetAttribLocation(shader_program, "color_input");
+	glVertexAttribPointer(color_in_attribute, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(color_in_attribute);
 }
 
 void View::render(SDL_Window* window) {
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glFlush();
+
 	SDL_GL_SwapWindow(window);
 }
 
